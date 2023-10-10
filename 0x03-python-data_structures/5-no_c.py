abce31ef1060ee6fa3b91d8f785a6e32ea368991
Amanz91 <findingmanzilla@gmail.com>
2023-10-10 06:31:45 -0700

    task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    if my_string:
+    if len(my_string) != 0:
         new = my_string.translate({ord(i): None for i in 'cC'})
         return new
+    else:
+        return my_string
