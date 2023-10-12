356c3c0271f691bcb38edc59062a46aecb02ed53
Amanz91 <findingmanzilla@gmail.com>
2023-10-12 07:41:04 -0700

    task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    new_list = set(my_list)
     new = 0
-    for i in range(len(my_list):
+    for i in range(len(new_list):
             new += i
     return new
