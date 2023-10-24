22da0a5eca294da974ec638b2b87e30679dd4cce
Amanz91 <findingmanzilla@gmail.com>
2023-10-24 07:06:40 -0700

    task 100 1

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -2,11 +2,10 @@
 import sys
 
 
-def safe_print_integer_err(value):
+def safe_function(fct, *args):
     try:
-        print("{:d}".format(value))
+        result = fct(*args)
+        return result
     except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
-        return False
-    else:
-        return True
+        return None
