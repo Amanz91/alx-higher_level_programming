65ae24465157c19c2f0294552bedeac839f74a1d
Amanz91 <findingmanzilla@gmail.com>
2023-10-25 07:13:10 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""A module to define a square"""
+
+
+class Square:
+    """A class to represent a square"""
+
+
+    def __init__(self, size):
+        """Constructor.
+
+        Args:
+            size: the length of a side of a square.
+        """
+        self.__size = size
