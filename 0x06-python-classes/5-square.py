78c6792c63436bd6f6fd6229662a853cb934cf8d
Amanz91 <findingmanzilla@gmail.com>
2023-10-27 08:48:35 -0700

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+"""A module to define a square"""
+
+
+class Square:
+    """A class to represent a square"""
+
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size (int): the length of a side of a square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Gets the size of a side of square.
+
+        Returns:
+            The size of the side.
+        """
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """Sets size of a side of a square.
+
+        Args:
+            size (int): the length of a side of a square.
+        Raises:
+            TypeError: if size is not an integer.
+            ValueError: if size is lessthan 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates area of a square.
+
+        Returns:
+            The size of the area.
+        """
+        return (self.__size ** 2)
+
+    def my_print(self):
+        """Prints a square."""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
