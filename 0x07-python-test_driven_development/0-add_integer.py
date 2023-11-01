7a1f9bbc8e7559bac07a6120cd00ad2e6d748cfb
Amanz91 <findingmanzilla@gmail.com>
2023-11-01 07:19:45 -0700

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -12,8 +12,8 @@ def add_integer(a, b=98):
     Returns:
         Sum.
     """
-    if ((not isinsatance(a, int) and not isinsatance(a, float))):
+    if ((not isinstance(a, int) and not isinstance(a, float))):
         raise TypeError("a must be an integer")
-    if ((not isinsatance(b, int) and not isinsatance(b, float))):
+    if ((not isinstance(b, int) and not isinstance(b, float))):
         raise TypeError("b must be an integer")
     return (int(a) + int(b))
