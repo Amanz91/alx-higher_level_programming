12fdecabcc51193a76aec17596a77ca652f578c4
Amanz91 <findingmanzilla@gmail.com>
2023-11-01 10:24:20 -0700

    task 2

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""A module to divide elements of a matrix"""
+
+
+def matrix_divided(matrix, div):
+    """A function to divide elements of matrix.
+
+    Args:
+        marix (int): matrix.
+        div (int): num to divide elemnts of the matrix.
+    Raises:
+        TypeError: if args aren't integers or float or matrix rows
+        size is not equal.
+        ZeroDivisionError: if 0 is element of matrix.
+    Returns:
+        Quotient.
+    """
+    if not isinstance(matrix, list) or len(matrix) == 0:
+        raise TypeError("matrix must be a matrix" + " (list of lists)"
+                        + " of integers/floats")
+    for r in matrix:
+        if not isinstance(r, list) or len(r) == 0:
+            raise TypeError("matrix must be a matrix (list of lists)" 
+                            + " of integers/floats")
+        if len(r) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the "
+                            + "same size")
+        for c in r:
+            if not isinstance(c, (int, float)):
+                raise TypeError("matrix must be a matrix (list of"
+                                + " lists) of integers/floats")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    return [[round(c / div, 2) for c in r] for r in matrix]
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
