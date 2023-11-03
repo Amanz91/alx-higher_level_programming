c341565d1af05858cd8332bd6054e999fcb6f2d2
Amanz91 <findingmanzilla@gmail.com>
2023-11-03 04:46:30 -0700

    task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""A module to print a square."""
+
+
+def print_square(size):
+    """A function to print a square.
+
+    Args:
+        size (int): size of square.
+    Raises:
+        TypeError: if size is not integer.
+        ValueError: if size is < 0.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(0, size):
+        [print("#", end="") for j in range(size)]
+        print("")
+    if size == 0:
+        print("")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")
