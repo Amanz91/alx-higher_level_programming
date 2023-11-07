bcfabd44bc83c1835f8babd74115d07976a61ede
Amanz91 <findingmanzilla@gmail.com>
2023-11-07 08:15:12 -0800

    t 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A module to open a file in append mode and append content."""
+
+
+def append_file(filename="", text=""):
+    """Func to open a file in append mode and write content.
+    Args:
+        filename (str): filename.
+        text (str): text to be written to file.
+    """
+    with open(filename, 'a', encoding="utf-8") as f:
+        return (f.write(text))
