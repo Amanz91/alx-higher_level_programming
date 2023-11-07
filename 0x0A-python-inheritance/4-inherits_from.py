e6e6dda47920fbaab39ac16eabd11ca5b000e47d
Amanz91 <findingmanzilla@gmail.com>
2023-11-07 04:47:30 -0800

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """A module checks object is exactly an instance of class."""
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """A function to check object's class."""
 
     if isinstance(obj, a_class) or type(obj) == a_class:
