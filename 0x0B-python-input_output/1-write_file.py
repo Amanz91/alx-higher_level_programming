ae0f5d2ba901668ca5a5af8ee28695e14e2eb570
Amanz91 <findingmanzilla@gmail.com>
2023-11-07 08:07:52 -0800

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -9,4 +9,4 @@ def write_file(filename="", text=""):
         text (str): text to be written to file.
     """
     with open(filename, encoding="utf-8") as f:
-        return (f.write(text), end="")
+        return (f.write(text))
