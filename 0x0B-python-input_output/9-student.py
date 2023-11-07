1fa64a1958731d5ee28269c1353422f50f263494
Amanz91 <findingmanzilla@gmail.com>
2023-11-07 09:18:56 -0800

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -9,7 +9,7 @@ class Student:
         """Initialize a student."""
         self.first_name = first_name
         self.last_name = last_name
-        sel.age = age
+        self.age = age
 
     def to_json(self):
         """Return the dictionary represntation of a student."""
