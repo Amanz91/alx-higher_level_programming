6fb750ba41959ad8b1d8300126b7755d9d85d35a
Amanz91 <findingmanzilla@gmail.com>
2023-11-13 04:44:19 -0800

    task 14

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,7 +3,7 @@
 from models.rectangle import Rectangle
 
 
-class Squae(Rectangle):
+class Square(Rectangle):
     """A sub class square of class rectangle."""
 
     def __init__(self, size, x=0, y=0, id=None):
